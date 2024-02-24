30da17a1c29cf606b0a99cfb651b3fc6b92e1823
xbh0403 <xbh0403@gmail.com>
2024-02-24 14:42:07 -0800

    Single Overlap Alignment

--- OverlapAlignment.py
+++ OverlapAlignment.py
@@ -6,8 +6,30 @@ sys.setrecursionlimit(100000)
 
 
 def OverlapVDJAlignment(match_reward: int, mismatch_penalty: int, indel_penalty: int,
-                        v_gene: str, j_gene: str, read: str, overlap_match_score: int, overlap_mismatch_score: int,
+                        overlap_match_score: int, overlap_mismatch_score: int,
+                        v_gene: Dict, j_gene: Dict, read: Dict, 
                         print_details) -> Tuple[int, str, str]:
+    """
+    Perform overlap alignment between V, D, and J genes and a read
+
+    Parameters
+    ----------
+    match_reward : int, reward for matching nucleotides
+    mismatch_penalty : int, penalty for mismatching nucleotides
+    indel_penalty : int, penalty for indels
+    overlap_match_score : int, reward for matching nucleotides in the overlap region
+    overlap_mismatch_score : int, penalty for mismatching nucleotides in the overlap region
+    v_gene : Dict, V gene
+    j_gene : Dict, J gene
+    read : Dict, read
+    print_details : bool, print details of the alignment
+
+    Returns
+    -------
+    Tuple[int, str, str, List, str, str, List], score, aligned V tail, aligned read head, aligned read tail, aligned J head
+    """
+    v_gene, j_gene, read = v_gene['gene'], j_gene['gene'], read['read']
+    v_gene_epi, j_gene_epi, read_epi = v_gene['epitopes'], j_gene['epitopes'], read['epitopes']
     score_v_tail, aligned_v_tail, aligned_read_head = OverlapAlignment(match_reward, mismatch_penalty, indel_penalty, v_gene, read, print_details)
     score_j_head, aligned_read_tail, aligned_j_head = OverlapAlignment(match_reward, mismatch_penalty, indel_penalty, read, j_gene, print_details)
     score_overlap_v, score_overlap_j = 0, 0
@@ -16,12 +38,28 @@ def OverlapVDJAlignment(match_reward: int, mismatch_penalty: int, indel_penalty:
     if len(aligned_read_tail) > 0 and len(aligned_j_head) > 0:
         score_overlap_j = sum([overlap_match_score if aligned_read_tail[i] == aligned_j_head[i] else overlap_mismatch_score for i in range(len(aligned_read_tail))])
     final_score = score_overlap_v + score_overlap_j
-    return final_score, aligned_v_tail, aligned_read_head, aligned_read_tail, aligned_j_head
+    return final_score, aligned_v_tail, aligned_read_head, v_gene_epi, aligned_read_tail, aligned_j_head, j_gene_epi
 
 
 def OverlapAlignment(match_reward: int, mismatch_penalty: int, indel_penalty: int,
                     s: str, t: str,
                     print_details) -> Tuple[int, str, str]:
+    """
+    Perform overlap alignment between two sequences
+    
+    Parameters
+    ----------
+    match_reward : int, reward for matching nucleotides
+    mismatch_penalty : int, penalty for mismatching nucleotides
+    indel_penalty : int, penalty for indels
+    s : str, first sequence
+    t : str, second sequence
+    print_details : bool, print details of the alignment
+    
+    Returns
+    -------
+    Tuple[int, str, str], score, aligned s, aligned t
+    """
     score_matrix, backtrack_matrix = [[0 for j in range(len(t)+1)] for i in range(len(s)+1)], [[0 for j in range(len(t)+1)] for i in range(len(s)+1)]
     max_score, max_i, max_j = float('-inf'), -1, -1
     for i in range(len(s)+1):
@@ -76,9 +114,12 @@ def OverlapAlignment(match_reward: int, mismatch_penalty: int, indel_penalty: in
 
 if __name__ == "__main__":
     match_reward, mismatch_penalty, indel_penalty = 1, 1, 1
-    v_gene = "CCAT"
-    read = "AT"
-    j_gene = "ATCC"
+    v_gene = "ZZZZZGCAT"
+    read = "ATNNNGC"
+    j_gene = "GCATYYYYY"
     overlap_match_score, overlap_mismatch_score = 3, 1
     print_details = False
-    print(OverlapVDJAlignment(match_reward, mismatch_penalty, indel_penalty, v_gene, j_gene, read, overlap_match_score, overlap_mismatch_score, print_details))
+    print(OverlapVDJAlignment(match_reward, mismatch_penalty, indel_penalty, 
+                              overlap_match_score, overlap_mismatch_score, 
+                              v_gene, j_gene, read, 
+                              print_details))
